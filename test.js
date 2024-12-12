299a913ab4090dd9a299a5d313918184bad4cb8e
vsriramakarthik <147160205+vsriramakarthik@users.noreply.github.com>
2024-12-12 10:48:10 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,3 @@
-// const { translateText, SUPPORTED_LANGUAGES } = require('./index');
-
-// (async () => {
-//   try {
-//     const text = 'మీ ఫోన్ లేదా కంప్యూటర్‌లో వ్యక్తిగత వ్యాఖ్యాత';
-//     const from = 'te'; 
-//     const to = 'en';   // Hindi
-
-//     console.log('Supported Languages:', SUPPORTED_LANGUAGES);
-//     const translatedText = await translateText(text, from, to);
-//     console.log(`Translation (${from} -> ${to}): ${translatedText}`);
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// })();
-
-
 const { translateText, SUPPORTED_LANGUAGES } = require('./index');
 
 (async () => {
